6810a28f59126c807a9b8928596c22cdeeee9f84
Kevin Cogan <kevin.s.cogan@gmail.com>
2022-04-06 23:52:33 +0100

    Added UI buttons to the app

---
+++ Phaze/CustomSettingTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  CustomSettingTableViewCell.swift
+//  Phaze
+//
+//  Created by Kevin Cogan on 06/04/2022.
+//  Copyright © 2022 UnKnown. All rights reserved.
+//
+
+import UIKit
+
+class CustomSettingTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+    @IBOutlet weak var cellView: UIView!
+    
+    @IBOutlet weak var settingsImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+
+}
