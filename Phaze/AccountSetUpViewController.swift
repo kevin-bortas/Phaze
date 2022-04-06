089aba8e54710a3135ca08e6d29e348f3e010dcc
Kevin Cogan <kevin.s.cogan@gmail.com>
2022-04-07 00:56:12 +0100

    Finished adding functionality to the weight gain section.

--- Phaze/AccountSetUpViewController.swift
+++ Phaze/AccountSetUpViewController.swift
@@ -2,29 +2,76 @@
 //  AccountSetUpViewController.swift
 //  Phaze
 //
-//  Created by Kevin Bortas on 06/04/2022.
+//  Created by Kevin Cogan on 27/01/2022.
 //  Copyright © 2022 UnKnown. All rights reserved.
 //
 
 import UIKit
 
-class AccountSetUpViewController: UIViewController {
+class AccountSetUpViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let elements = ["Delete Account", "Privacy & Security", "Help & Support", "About", "Logout"]
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    var index: Int?
+    
     override func viewDidLoad() {
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You selected \(elements[indexPath.row]).")
+        
+        if (elements[indexPath.row] == "Delete Account") {
+            UIApplication.shared.open(URL(string: "https://www.programiz.com/swift-programming/if-else-statement#:~:text=The%20if...else%20statement%20evaluates%20the%20condition%20inside%20the,code%20inside%20else%20is%20skipped")!, options: [:], completionHandler: nil)
+        }
+        
+        if (elements[indexPath.row] == "Privacy & Security") {
+            UIApplication.shared.open(URL(string: "https://www.privacypolicies.com/live/1262b0a2-1e18-4c19-81f2-49d6c51b9f46")!, options: [:], completionHandler: nil)
+        }
+        
+        if (elements[indexPath.row] == "Help & Support") {
+            UIApplication.shared.open(URL(string: "http://website-env-1.eba-3evzsc6b.eu-west-1.elasticbeanstalk.com/#contact")!, options: [:], completionHandler: nil)
+        }
+        
+        if (elements[indexPath.row] == "About") {
+            UIApplication.shared.open(URL(string: "http://website-env-1.eba-3evzsc6b.eu-west-1.elasticbeanstalk.com/#about")!, options: [:], completionHandler: nil)
+        }
+        
+        if (elements[indexPath.row] == "Logout") {
+            UIApplication.shared.open(URL(string: "https://www.programiz.com/")!, options: [:], completionHandler: nil)
+        }
+        
+        
     }
-    */
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return elements.count
+    }
+
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 100
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customSettingCell") as! CustomSettingTableViewCell
+
+        cell.cellView.layer.cornerRadius = cell.cellView.frame.height / 2
+
+        cell.settingsLabel.text = elements[indexPath.row]
+        cell.settingsImage.image = UIImage(named: elements[indexPath.row])
+//        cell.settingsImage.layer.cornerRadius = cell.settingsImage.frame.height / 2
+
+        return cell
+    }
+    
 }
+
+
---
+++ Phaze/CustomWeightPlanTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  CustomWeightPlanTableViewCell.swift
+//  Phaze
+//
+//  Created by Kevin Cogan on 07/04/2022.
+//  Copyright © 2022 UnKnown. All rights reserved.
+//
+
+import UIKit
+
+class CustomWeightPlanTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var cellView: UIView!
+    
+    @IBOutlet weak var weightImage: UIImageView!
+    
+    
+    @IBOutlet weak var weightLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
