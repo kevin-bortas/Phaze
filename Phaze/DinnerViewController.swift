7cb7abf09df1627fe3205f4f399b1ecddb5fbf9a
Kevin Cogan <kevin.s.cogan@gmail.com>
2022-04-10 23:07:21 +0100

    Created pages to see meals added.

---
+++ Phaze/DinnerViewController.swift
@@ -0,0 +1,24 @@
+//
+//  DinnerViewController.swift
+//  Phaze
+//
+//  Created by Kevin Cogan on 10/04/2022.
+//  Copyright © 2022 UnKnown. All rights reserved.
+//
+
+import UIKit
+
+class DinnerViewController: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Phaze/LunchViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LunchViewController.swift
+//  Phaze
+//
+//  Created by Kevin Cogan on 10/04/2022.
+//  Copyright © 2022 UnKnown. All rights reserved.
+//
+
+import UIKit
+
+class LunchViewController: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
